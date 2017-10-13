d1aeab26a0ad127485b86f4bc90d87cb9c6bd81d
zbroja <maciejpielizg@gmail.com>
2017-10-13 18:28:22 +0200

    message bug

--- src/main/java/pl/pielizg/messageExchanger/service/ExchangeServiceImpl.java
+++ src/main/java/pl/pielizg/messageExchanger/service/ExchangeServiceImpl.java
@@ -30,7 +30,7 @@ public class ExchangeServiceImpl implements ExchangeService {
 
     @Override
     public Container setMessage(String message) {
-        if(!message.matches("[0-9a-zA-Z]+#[0-9a-zA-Z]+#[0-9a-zA-Z]+")){
+        if(!message.matches("[0-9a-zA-Z]+#[0-9a-zA-Z]+#[0-9a-zA-Z ]+")){
             return new Container("Źle skonstruowana wiadomość");
         }
         else {
